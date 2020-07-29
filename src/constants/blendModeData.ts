81d1ffe75fc5614fda889065d36fc9002d5b79b9
youkan <youkan94@gmail.com>
2020-07-30 02:05:25 +0900

    feat: 描画モードのオブジェクト・取得用関数の作成

---
+++ src/constants/blendModeData.ts
@@ -0,0 +1,430 @@
+/**
+ * @see: https://twitter.com/optie_f/status/974263251071418368
+ * @todo: existの箇所の対応(今はすべてtrueになってるので意味がない)
+ */
+const blemdModeData = {
+    normal: {
+        mode: `normal`,
+        type: {
+            base: `other`,
+            brightness: null,
+            math: null,
+        },
+        name: {
+            ja: `通常`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    lighten: {
+        mode: `lighten`,
+        type: {
+            base: `normal`,
+            brightness: `+`,
+            math: `IndividualComparison`,
+        },
+        name: {
+            ja: `比較(明)`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    screen: {
+        mode: `screen`,
+        type: {
+            base: `normal`,
+            brightness: `+`,
+            math: `Multiplication`,
+        },
+        name: {
+            ja: `スクリーン`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    linearDodge: {
+        mode: `linearDodge`,
+        type: {
+            base: `normal`,
+            brightness: `+`,
+            math: `Addition`,
+        },
+        name: {
+            equalJp: `加算`,
+            ja: `覆い焼きリニア(加算)`,
+            equalEn: `Add`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    colorDodge: {
+        mode: `colorDodge`,
+        type: {
+            base: `normal`,
+            brightness: `+`,
+            math: `Division`,
+        },
+        name: {
+            ja: `覆い焼きカラー`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    lighterColor: {
+        mode: `lighterColor`,
+        type: {
+            base: `normal`,
+            brightness: `+`,
+            math: `TotalComparison`,
+        },
+        name: {
+            ja: `カラー比較(明)`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    pinLight: {
+        mode: `pinLight`,
+        type: {
+            base: `normal`,
+            brightness: `+-`,
+            math: `IndividualComparison`,
+        },
+        name: {
+            ja: `ピンライト`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    overlay: {
+        mode: `overlay`,
+        type: {
+            base: `normal`,
+            brightness: `+-`,
+            math: `Multiplication`,
+        },
+        name: {
+            ja: `オーバーレイ`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    linearLight: {
+        mode: `linearLight`,
+        type: {
+            base: `normal`,
+            brightness: `+-`,
+            math: `Addition`,
+        },
+        name: {
+            ja: `オーバーレイ`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    vividLight: {
+        mode: `vividLight`,
+        type: {
+            base: `normal`,
+            brightness: `+-`,
+            math: `Division`,
+        },
+        name: {
+            ja: `ビビッドライト`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    softLight: {
+        mode: `softLight`,
+        type: {
+            base: `normal`,
+            brightness: `+-`,
+            math: `TotalComparison`,
+        },
+        name: {
+            ja: `ソフトライト`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    hardLight: {
+        mode: `hardLight`,
+        type: {
+            base: `normal`,
+            brightness: `+-`,
+            math: `Multiplication`,
+        },
+        name: {
+            ja: `ハードライト`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    hardMix: {
+        mode: `hardMix`,
+        type: {
+            base: `normal`,
+            brightness: `+-`,
+            math: `Division`,
+        },
+        name: {
+            ja: `ハードミックス`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    darken: {
+        mode: `darken`,
+        type: {
+            base: `normal`,
+            brightness: `-`,
+            math: `IndividualComparison`,
+        },
+        name: {
+            ja: `比較(暗)`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    multiply: {
+        mode: `multiply`,
+        type: {
+            base: `normal`,
+            brightness: `-`,
+            math: `Multiplication`,
+        },
+        name: {
+            ja: `乗算`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    linearBurn: {
+        mode: `linearBurn`,
+        type: {
+            base: `normal`,
+            brightness: `-`,
+            math: `Addition`,
+        },
+        name: {
+            ja: `焼き込みリニア`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    colorBurn: {
+        mode: `colorBurn`,
+        type: {
+            base: `normal`,
+            brightness: `-`,
+            math: `Division`,
+        },
+        name: {
+            ja: `焼き込みカラー`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    darkerColor: {
+        mode: `darkerColor`,
+        type: {
+            base: `normal`,
+            brightness: `-`,
+            math: `TotalComparison`,
+        },
+        name: {
+            ja: `カラー比較(暗)`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    hue: {
+        mode: `hue`,
+        type: {
+            base: `hsl`,
+            brightness: null,
+            math: null,
+        },
+        name: {
+            ja: `色相`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+        ready: false,
+    },
+    color: {
+        mode: `color`,
+        type: {
+            base: `hsl`,
+            brightness: null,
+            math: null,
+        },
+        name: {
+            ja: `カラー`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+        ready: false,
+    },
+    saturation: {
+        mode: `saturation`,
+        type: {
+            base: `hsl`,
+            brightness: null,
+            math: null,
+        },
+        name: {
+            ja: `彩度`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+        ready: false,
+    },
+    luminosity: {
+        mode: `luminosity`,
+        type: {
+            base: `hsl`,
+            brightness: null,
+            math: null,
+        },
+        name: {
+            ja: `輝度`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+        ready: false,
+    },
+    difference: {
+        mode: `difference`,
+        type: {
+            base: `math`,
+            brightness: null,
+            math: null,
+        },
+        name: {
+            ja: `差の絶対値`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    exclusion: {
+        mode: `exclusion`,
+        type: {
+            base: `math`,
+            brightness: null,
+            math: null,
+        },
+        name: {
+            ja: `除外`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    subtract: {
+        mode: `subtract`,
+        type: {
+            base: `math`,
+            brightness: null,
+            math: null,
+        },
+        name: {
+            ja: `減算`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+    divide: {
+        mode: `divide`,
+        type: {
+            base: `math`,
+            brightness: null,
+            math: null,
+        },
+        name: {
+            ja: `除算`,
+        },
+        exist: {
+            adobe: true,
+            css: true,
+            blender: true,
+        },
+    },
+};
+
+export default blemdModeData;
---
+++ src/utils/GetBlendModeData.ts
@@ -0,0 +1,93 @@
+import BlendModeData from '../constants/blendModeData';
+
+/**
+ * すでに準備が整っている描画モードを取得する
+ */
+export const getReadyBlendMode = ( blendModeData:any ) => {
+    const resultObject:any = {};
+    for(let k of Object.keys(blendModeData)) {
+        if( blendModeData[k].ready !== false ){
+            resultObject[k] = blendModeData[k];
+        }
+    }
+    return resultObject;
+}
+
+/**
+ * getReadyBlendMode関数の返り値。準備が出来ているもののobjectのみ取得。
+ */
+export const readyBlendModeData = getReadyBlendMode(BlendModeData);
+
+/**
+ * reduxなどで管理する描画モードのオンオフのオブジェクトを生成する
+ * @param blendModeData 描画モードのデータオブジェクト
+ * @param invertBlendModeArray デフォルトブール値を値が違う描画モードを配列にして文字を入れる
+ * @param defaultBooleanValue デフォルトのbool値
+ */
+export const getBoolStateBlendObject = ( blendModeData:any, invertBlendModeArray: string[] = [], defaultBooleanValue: boolean = false ) => {
+    let resultObject:any = {};
+    let insertObject:any = {};
+    for(let k of Object.keys(blendModeData)) {
+        if( blendModeData[k].ready !== false ){
+            resultObject[k] = defaultBooleanValue;
+        }
+    }
+
+    invertBlendModeArray.forEach((blendModeName: string) => {
+        insertObject[blendModeName] = !defaultBooleanValue;
+    })
+
+    resultObject = {
+        ...resultObject,
+        ...insertObject,
+    }
+    return resultObject;
+};
+
+/**
+ * 一つだけのtrueの値をもつオブジェクトかどうかを判別する
+ * @param boolStateBlendObject 
+ */
+export const isSingleTrueStateObject = (boolStateBlendObject:any) => {
+    let boolTrueCount: number = 0;
+    for(let k of Object.keys(boolStateBlendObject)) {
+        if( boolStateBlendObject[k] === true ){
+            boolTrueCount += 1;
+            if( boolTrueCount >= 2 ){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+export const getTrueStateBlendNameArray = (boolStateBlendObject:any) => {
+    let boolTrueNameArray: string[] = [];
+    for(let k of Object.keys(boolStateBlendObject)) {
+        if( boolStateBlendObject[k] === true ){
+            boolTrueNameArray.push(k);
+        }
+    }
+    return boolTrueNameArray;
+}
+
+/**
+ * 
+ * @param blendModeData 
+ * @param onlyReady 
+ */
+export const getBlendModeArray = (blendModeData: any, onlyReady: boolean = true) => {
+    const resultArray = [];
+    for(let k of Object.keys(blendModeData)) {
+        resultArray.push(blendModeData[k]);
+    }
+    if( onlyReady ){
+        return resultArray.filter((singleBlendModeData) => {
+            return singleBlendModeData.ready === true || singleBlendModeData.ready == null;
+        });
+    }
+    return resultArray;
+}
+
+export const readyBlendModeArray = getBlendModeArray(BlendModeData,true);
+export const blendModeArray = getBlendModeArray(BlendModeData,false);
