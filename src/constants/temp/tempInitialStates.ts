eaaf1f4d0b14b313021e33999bf622116509021a
youkan <youkan94@gmail.com>
2020-07-30 02:15:44 +0900

    feat: 開発用のサンプルステートオブジェクトファイルを追加

---
+++ src/constants/temp/tempInitialStates.ts
@@ -0,0 +1,174 @@
+import { image01, image02, image03 } from './tempImageData';
+
+export const glAllLayerDataStateItem = [
+    [
+        {
+            name: `example`,
+            type: `image`,
+            opacity: 1.0,
+            image: {
+                type: `normal`,
+                source: image01,
+                originalWidth: 469,
+                originalHeight: 463,
+                mime: `image/jpeg`,
+            },
+            blendMode: `normal`,
+            singleColor: null,
+            layerNumber: 0,
+            isBottomLayer: true,
+        },
+        {
+            name: `example`,
+            type: `image`,
+            opacity: 1.0,
+            image: {
+                type: `cover`,
+                source: image03,
+                originalWidth: 1024,
+                originalHeight: 640,
+                mime: `image/jpeg`,
+            },
+            blendMode: `overlay`,
+            singleColor: null,
+            layerNumber: 1,
+            isBottomLayer: false,
+        }
+    ],
+    [
+        {
+            name: `example`,
+            type: `image`,
+            opacity: 1.0,
+            image: {
+                type: `normal`,
+                source: image01,
+                originalWidth: 469,
+                originalHeight: 463,
+                mime: `image/jpeg`,
+            },
+            blendMode: `normal`,
+            singleColor: null,
+            layerNumber: 0,
+            isBottomLayer: true,
+        },
+        {
+            name: `example`,
+            type: `image`,
+            opacity: 1.0,
+            image: {
+                type: `cover`,
+                source: image03,
+                originalWidth: 1024,
+                originalHeight: 640,
+                mime: `image/jpeg`,
+            },
+            blendMode: `multiply`,
+            singleColor: null,
+            layerNumber: 1,
+            isBottomLayer: false,
+        }
+    ],
+    [
+        {
+            name: `example`,
+            type: `image`,
+            opacity: 1.0,
+            image: {
+                type: `normal`,
+                source: image01,
+                originalWidth: 469,
+                originalHeight: 463,
+                mime: `image/jpeg`,
+            },
+            blendMode: `normal`,
+            singleColor: null,
+            layerNumber: 0,
+            isBottomLayer: true,
+        },
+        {
+            name: `example`,
+            type: `image`,
+            opacity: 1.0,
+            image: {
+                type: `cover`,
+                source: image03,
+                originalWidth: 1024,
+                originalHeight: 640,
+                mime: `image/jpeg`,
+            },
+            blendMode: `screen`,
+            singleColor: null,
+            layerNumber: 1,
+            isBottomLayer: false,
+        }
+    ],
+    [
+        {
+            name: `example`,
+            type: `image`,
+            opacity: 1.0,
+            image: {
+                type: `normal`,
+                source: image01,
+                originalWidth: 469,
+                originalHeight: 463,
+                mime: `image/jpeg`,
+            },
+            blendMode: `normal`,
+            singleColor: null,
+            layerNumber: 0,
+            isBottomLayer: true,
+        },
+        {
+            name: `example`,
+            type: `image`,
+            opacity: 1.0,
+            image: {
+                type: `cover`,
+                source: image03,
+                originalWidth: 1024,
+                originalHeight: 640,
+                mime: `image/jpeg`,
+            },
+            blendMode: `colorDodge`,
+            singleColor: null,
+            layerNumber: 1,
+            isBottomLayer: false,
+        }
+    ],
+    [
+        {
+            name: `example`,
+            type: `image`,
+            opacity: 1.0,
+            image: {
+                type: `normal`,
+                source: image01,
+                originalWidth: 469,
+                originalHeight: 463,
+                mime: `image/jpeg`,
+            },
+            blendMode: `normal`,
+            singleColor: null,
+            layerNumber: 0,
+            isBottomLayer: true,
+        },
+        {
+            name: `example`,
+            type: `image`,
+            opacity: 1.0,
+            image: {
+                type: `cover`,
+                source: image03,
+                originalWidth: 1024,
+                originalHeight: 640,
+                mime: `image/jpeg`,
+            },
+            blendMode: `colorBurn`,
+            singleColor: null,
+            layerNumber: 1,
+            isBottomLayer: false,
+        }
+    ],
+];
