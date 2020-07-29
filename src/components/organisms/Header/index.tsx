42f0e6facde7032340b7a67236f96f014e6c6726
youkan <youkan94@gmail.com>
2020-07-30 02:04:07 +0900

    feat: ヘッダー要素の作成

---
+++ src/components/organisms/Header/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+export default (props: any) => {
+    
+    
+    
+    const {themeSettings} = props;
+    
+    const useStyles = makeStyles({
+        root: {
+            minHeight: themeSettings.header.appBarHeight,
+        }
+    });
+    const classes = useStyles();
+
+    return (
+        <AppBar position="fixed" className={classes.root}>
+            <Toolbar className={classes.root}>
+                <Typography>
+                    BlendMonitoring
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    );
+}
