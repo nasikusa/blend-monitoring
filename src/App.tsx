ddec926cb027b0f7f036cb90d7e8ee2583779eac
youkan <youkan94@gmail.com>
2020-07-29 23:16:13 +0900

    feat: Material UIの設定を追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,48 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import FoundationStyle from './styles/foundation';
+import { createMuiTheme, ThemeProvider  } from '@material-ui/core/styles';
+import teal from '@material-ui/core/colors/teal';
+import cyan from '@material-ui/core/colors/cyan';
+import Top from './components/pages/Top';
+import EditContainer from './container/EditContainer';
+
+const theme = createMuiTheme({
+    palette: {
+      type: 'dark',
+      primary: teal,
+      secondary: cyan,
+    },
+    typography: {
+      fontSize: 12,
+      fontFamily: [
+        '"Noto Sans JP"',
+        'sans-serif',
+      ].join(','),
+      button: {
+        textTransform: "none"
+      },
+    },
+    props: {
+      MuiList: {
+          dense: true,
+      },
+      MuiCheckbox: {
+        color: "primary"
+      },
+      MuiRadio: {
+          color: "primary"
+      },
+      MuiSwitch: {
+          color: "primary"
+      },
+    }
+});
 
 function App() {
   return (
     <div className="App">
+      <ThemeProvider theme={theme}>
         <FoundationStyle />
         <BrowserRouter>
           <Switch>
@@ -13,6 +51,7 @@ function App() {
             <Route path="/edit" component={EditContainer} />
           </Switch>
         </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
