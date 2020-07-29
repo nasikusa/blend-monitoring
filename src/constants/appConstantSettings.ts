689f091cdf896851340fc73b35013a4a0e195ddb
youkan <youkan94@gmail.com>
2020-07-30 02:12:28 +0900

    feat: アプリの不変のデータ情報ファイルを追加

---
+++ src/constants/appConstantSettings.ts
@@ -0,0 +1,8 @@
+export const AppName = `BlendMonitoring`;
+
+export const DeveloperInfo = {
+    name: `nasikusa`,
+    subName: `youkan`,
+    github: `nasikusa`,
+    twitter: `nakanasinokusa`,
+};
