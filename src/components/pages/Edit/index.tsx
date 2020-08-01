2ba31d4f26a5a3d8949866ce282195e6ee14e88d
youkan <youkan94@gmail.com>
2020-08-02 00:05:51 +0900

    fix: リサイズによるglslアイテムのサイズ変更機能のバグを修正

--- src/components/pages/Edit/index.tsx
+++ src/components/pages/Edit/index.tsx
@@ -1,7 +1,6 @@
 import React, { useRef } from 'react';
 import EventListener from 'react-event-listener';
 import { RemoveScroll } from 'react-remove-scroll';
-// import { useSelector, useDispatch } from 'react-redux';
 
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -11,17 +10,15 @@ import GlEdit from '../../organisms/GlEdit';
 import PageTemplateContainer from '../../../container/PageTemplateContainer';
 import GlBoxContainer from '../../../container/GlBoxContainer';
 
-// import updateSingleItemSize from '../../../stores/glSettings';
-
 import { ThemeSettingsType } from '../../../stores/themeSettings';
 
 export type Props = {
   themeSettings: ThemeSettingsType;
+  updateSingleItemSize: any;
 };
 
 const Edit = (props: Props) => {
-  const { themeSettings } = props;
-  // const dispatch = useDispatch();
+  const { themeSettings, updateSingleItemSize } = props;
   const useStyles = makeStyles(() => ({
     scrollable: {
       overflowY: `scroll`,
@@ -30,17 +27,11 @@ const Edit = (props: Props) => {
   }));
   const classes = useStyles();
   const containerRef = useRef(null);
-  // @ts-ignore
-  // const glSettings = useSelector((state) => state.glSettings);
-
-  // const handleResize = (ref: any) => {
-  //   dispatch(
-  //     updateSingleItemSize({
-  //       type: 'updateSingleItemSize',
-  //       glBoxClientWidth: ref.current.clientWidth,
-  //     });
-  //   );
-  // };
+  const handleResize = (ref: any) => {
+    updateSingleItemSize({
+      glBoxClientWidth: ref.current.clientWidth,
+    });
+  };
 
   const pageBody = (
     <RemoveScroll>
@@ -53,9 +44,9 @@ const Edit = (props: Props) => {
         </Grid>
         <EventListener
           target="window"
-          // onResize={() => {
-          //   handleResize(containerRef);
-          // }}
+          onResize={() => {
+            handleResize(containerRef);
+          }}
         />
       </Box>
     </RemoveScroll>
