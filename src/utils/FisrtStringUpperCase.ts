ce235877e8a5d9ae1dd851bfcba470dcee3f630a
youkan <youkan94@gmail.com>
2020-08-02 14:52:30 +0900

    fix: 空文字列が入ったときの対応

--- src/utils/FisrtStringUpperCase.ts
+++ src/utils/FisrtStringUpperCase.ts
@@ -1,6 +1,9 @@
 /**
  * 英語文字列の最初の文字を大文字に変換して返す関数
  */
-export default (inputString: string): string => {
-  return inputString.charAt(0).toUpperCase() + inputString.slice(1);
+export default (inputString: string = ''): string => {
+  if (inputString.length > 0) {
+    return inputString.charAt(0).toUpperCase() + inputString.slice(1);
+  }
+  return '';
 };
