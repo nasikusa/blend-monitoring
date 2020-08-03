11d67cca21a5774fbcc725193d7df398d63f47a0
youkan <youkan94@gmail.com>
2020-08-04 02:31:06 +0900

    feat: 透過度スライダーコンポーネントの機能追加

--- src/components/molecules/OpacitySlider/index.tsx
+++ src/components/molecules/OpacitySlider/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Slider from '@material-ui/core/Slider';
 import Grid from '@material-ui/core/Grid';
@@ -8,7 +8,17 @@ import Input from '@material-ui/core/Input';
 
 import OpacityIcon from '@material-ui/icons/Opacity';
 
-import { GlCollectionOrderContext } from '../Collections';
+import {
+  GlCollectionInterfaceArray,
+  GlCollectionInterface,
+} from '../../../stores/collectionData';
+
+type Props = {
+  updateOpacity: any;
+  collectionData: GlCollectionInterfaceArray;
+  globalStateOpacityData: GlCollectionInterface['opacity'];
+  glCollectionOrderKey: number;
+};
 
 /**
  * mateiral uiのカスタムスタイル
@@ -26,27 +36,64 @@ const useStyles = makeStyles({
  * 透過度のスライダーコンポーネント
  * @todo Opacityだけでなく汎用性をもたせたい
  */
-const OpacitySlider: React.FC = (props: any) => {
-  // const [opacity, setOpacity] = React.useState<
-  //   number | string | Array<number | string>
-  // >(100);
+const OpacitySlider: React.FC<Props> = (props: Props) => {
   const classes = useStyles();
-  const { updateOpacity, collectionData } = props;
-  const glCollectionOrderKey = useContext(GlCollectionOrderContext);
-  const globalStateOpacityValue = collectionData[glCollectionOrderKey].opacity;
+  const { updateOpacity, globalStateOpacityData, glCollectionOrderKey } = props;
+  /**
+   * デフォルトのカラーをグローバルのstateから取得する
+   */
+  const defaultOpacityValue = (): number => {
+    if (Array.isArray(globalStateOpacityData)) {
+      return globalStateOpacityData[0] * 100;
+    }
+    if (
+      typeof globalStateOpacityData === 'number' &&
+      !Number.isNaN(globalStateOpacityData)
+    ) {
+      return globalStateOpacityData * 100;
+    }
+    return 100;
+  };
+  const [opacityState, setOpacityState] = useState(defaultOpacityValue);
+  const [tempOnChangeState, setTempOnChangeState] = useState(false);
+  const [tempOnChangeCommitState, setTempOnChangeCommitState] = useState(false);
+  const globalStateOpacityValue = globalStateOpacityData;
 
   /**
-   * スライダー変更時のイベント
+   * スライダー変更時のイベント。ローカルステートのみ変更する。
    * @param event
    * @param newValue 透過度の値
+   * @todo 配列データが入ってきた際の対応(opacityが複数のパターンがある場合)
    */
-  const handleChange = (event: any, value: number | number[]) => {
+  const handleChange = (
+    event: React.ChangeEvent<{}>,
+    eventValue: number | number[]
+  ) => {
+    if (!Array.isArray(eventValue)) {
+      setOpacityState(eventValue);
+    }
+    setTempOnChangeCommitState(false);
+    setTempOnChangeState(true);
+  };
+
+  /**
+   * 透過度が確定した際にdispatchを実行する
+   * @param event
+   * @param value 透過度の値
+   * @todo 配列データが入ってきた際の対応(opacityが複数のパターンがある場合)
+   */
+  const handleChangeCommitted = (
+    event: React.ChangeEvent<{}>,
+    value: number | number[]
+  ) => {
     if (!Array.isArray(value)) {
       updateOpacity({
-        opacityValue: value * 0.01,
+        opacityValue: opacityState * 0.01,
         glCollectionOrderKey,
       });
     }
+    setTempOnChangeCommitState(true);
+    setTempOnChangeState(false);
   };
 
   /**
@@ -54,7 +101,7 @@ const OpacitySlider: React.FC = (props: any) => {
    * @param event
    */
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    // setOpacity(event.target.value === '' ? '' : Number(event.target.value));
+    setOpacityState(event.target.value === '' ? 0 : Number(event.target.value));
     updateOpacity({
       opacityValue:
         event.target.value === '' ? '' : Number(event.target.value) * 0.01,
@@ -67,19 +114,38 @@ const OpacitySlider: React.FC = (props: any) => {
    */
   const handleBlur = () => {
     if (globalStateOpacityValue < 0) {
-      // setOpacity(0);
       updateOpacity({
         opacityValue: 0.0,
         glCollectionOrderKey,
       });
     } else if (globalStateOpacityValue > 100) {
-      // setOpacity(100);
       updateOpacity({
         opacityValue: 1.0,
         glCollectionOrderKey,
       });
     }
   };
+
+  /**
+   * スライダーを一定時間保持しつづけるとreduxのglobalのstateが更新される関数
+   */
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (tempOnChangeState === true && tempOnChangeCommitState === false) {
+        updateOpacity({
+          opacityValue: opacityState * 0.01,
+          glCollectionOrderKey,
+        });
+      }
+    }, 300);
+    return () => clearTimeout(timer);
+  }, [
+    tempOnChangeState,
+    tempOnChangeCommitState,
+    glCollectionOrderKey,
+    updateOpacity,
+    opacityState,
+  ]);
 
   return (
     <Box width={1}>
@@ -92,22 +158,24 @@ const OpacitySlider: React.FC = (props: any) => {
         </Grid>
         <Grid item xs>
           <Slider
-            value={
-              typeof globalStateOpacityValue === 'number'
-                ? Math.floor(globalStateOpacityValue * 100)
-                : 0
-            }
+            value={opacityState}
             min={0}
             max={100}
+            defaultValue={100}
             valueLabelDisplay="auto"
             onChange={handleChange}
+            onChangeCommitted={handleChangeCommitted}
             aria-labelledby="opacity-slider"
           />
         </Grid>
         <Grid item>
           <Input
             className={classes.input}
-            value={Math.floor(globalStateOpacityValue * 100)}
+            value={
+              typeof globalStateOpacityValue === 'number'
+                ? Math.floor(globalStateOpacityValue * 100)
+                : null
+            }
             margin="dense"
             onChange={handleInputChange}
             onBlur={handleBlur}
--- src/container/CustomSliderContainer.tsx
+++ src/container/CustomSliderContainer.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import OpacitySlider from '../components/molecules/OpacitySlider';
 
 import { updateOpacity as updateOpacityAction } from '../stores/collectionData';
 import { AppState } from '../stores/index';
+import { GlCollectionOrderContext } from '../components/molecules/Collections';
 
 export default (props: any) => {
   const collectionData = useSelector((state: AppState) => state.collectionData);
+  const glCollectionOrderKey = useContext(GlCollectionOrderContext);
+  const globalStateOpacityData = collectionData[glCollectionOrderKey].opacity;
 
   const dispatch = useDispatch();
 
@@ -17,7 +20,13 @@ export default (props: any) => {
     [dispatch]
   );
 
-  const combineProps = { collectionData, updateOpacity, ...props };
+  const combineProps = {
+    collectionData,
+    updateOpacity,
+    glCollectionOrderKey,
+    globalStateOpacityData,
+    ...props,
+  };
 
   return <OpacitySlider {...combineProps} />;
 };
