64a00c2e1978c5140bd1864d34e8728d0522efbe
youkan <youkan94@gmail.com>
2020-08-04 02:33:31 +0900

    chore: 開発用の追跡ツールを追加

---
+++ src/wdyr.tsx
@@ -0,0 +1,9 @@
+/*eslint-disable*/
+import React from 'react';
+
+if (process.env.NODE_ENV === 'development') {
+  const whyDidYouRender = require('@welldone-software/why-did-you-render');
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
+  });
+}
