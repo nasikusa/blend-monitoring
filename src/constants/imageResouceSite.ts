86c7de7661c85fac6078ec5726fc6609c3a71db7
youkan <youkan94@gmail.com>
2020-08-07 13:23:15 +0900

    feat: テクスチャ画像サイトの情報のconstantを追加

---
+++ src/constants/imageResouceSite.ts
@@ -0,0 +1,48 @@
+export const imageResouceSiteArray = {
+  Pixabay: {
+    name: 'Pixabay',
+    url: 'https://pixabay.com/',
+  },
+  Unsplash: {
+    name: 'Unsplash',
+    url: 'https://unsplash.com/',
+  },
+  'BEIZ images': {
+    name: 'BEIZ images',
+    url: 'https://www.beiz.jp/',
+  },
+  'Texture Haven': {
+    name: 'Texture Haven',
+    url: 'https://texturehaven.com/',
+  },
+  'textures.com': {
+    name: 'textures.com',
+    url: 'https://www.textures.com/',
+  },
+  'Texture Ninja': {
+    name: 'Texture Ninja',
+    url: 'https://texture.ninja/',
+  },
+  Texturelib: {
+    name: 'Texturelib',
+    url: 'http://texturelib.com/',
+  },
+  'CC0 Textures': {
+    name: 'CC0 Textures',
+    url: 'https://cc0textures.com/',
+  },
+  'Share Textures': {
+    name: 'Share Textures',
+    url: 'https://www.sharetextures.com/',
+  },
+  PAKUTASO: {
+    name: 'PAKUTASO',
+    url: 'https://www.pakutaso.com/',
+  },
+  'Paper-co': {
+    name: 'Paper-co',
+    url: 'https://free-paper-texture.com/',
+  },
+};
+
+export default imageResouceSiteArray;
