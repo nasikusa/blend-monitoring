4b277a83a54a9672cedf35c91a05a4202cb1051e
masahiro <youkan94@gmail.com>
2020-08-20 20:48:37 +0900

    refactor: カスタムツールチップのカラーを変更

--- src/components/atoms/CustomTooltip/index.tsx
+++ src/components/atoms/CustomTooltip/index.tsx
@@ -1,10 +1,13 @@
 import Tooltip from '@material-ui/core/Tooltip';
 import { withStyles, Theme } from '@material-ui/core/styles';
 
+/**
+ * カスタマイズされたツールチップコンポーネント
+ */
 const CustomTooltip = withStyles((theme: Theme) => ({
   tooltip: {
-    backgroundColor: theme.palette.common.white,
-    color: 'rgba(0, 0, 0, 0.87)',
+    backgroundColor: theme.palette.text.primary,
+    color: theme.palette.background.default,
     boxShadow: theme.shadows[1],
     fontSize: 11,
   },
