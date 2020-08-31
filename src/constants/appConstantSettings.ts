c6571670291aad2fb75114ff0e6553e36ef10982
youkan <youkan94@gmail.com>
2020-09-01 04:36:14 +0900

    v0.1.6

--- src/constants/appConstantSettings.ts
+++ src/constants/appConstantSettings.ts
@@ -11,8 +11,8 @@ export const AppNameHasAccent = isBeta ? `${AppName} β` : `${AppName}`;
 export const AppSiteInfo = {
   url: 'https://blend.nasikusa.net',
   shortenURL: 'https://bit.ly/33ZGL2i',
-  version: '0.1.5',
-  updatedAt: '2020 / 08 / 29',
+  version: '0.1.6',
+  updatedAt: '2020 / 09 / 01',
   createdAt: '2020 / 07 / 25',
   description: `
   ${AppName}は
