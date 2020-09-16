26f5d5a3986819e576bbe1f2149f7ce7cda458fb
nasikusa <youkan94@gmail.com>
2020-09-17 05:23:04 +0900

    refactor: ColorBoxコンポーネントをリファクタ

--- src/components/atoms/ColorBox/ColorBox.tsx
+++ src/components/atoms/ColorBox/ColorBox.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box, { BoxProps } from '@material-ui/core/Box';
 import { css, SerializedStyles } from '@emotion/core';
+
+type activeStyleTypeType = 'none' | 'scale' | 'opacity' | 'border';
 
 type Props = Readonly<
   Partial<{
@@ -10,28 +12,69 @@ type Props = Readonly<
     borderWidth: number;
     boxSize: 'small' | 'medium' | 'large' | 'exLarge';
     boxSizeHeightRatio: number;
-    boxSpacing: number;
     onClick: () => void;
     children: never;
     muiBoxProps: BoxProps;
+    innerMuiBoxProps: BoxProps;
+    active: boolean;
+    activeStyleType:
+      | activeStyleTypeType
+      | [activeStyleTypeType, ...activeStyleTypeType[]];
   }>
 >;
+
+export const styles: { [K: string]: SerializedStyles } = {
+  cursorDefault: css({
+    cursor: 'default',
+  }),
+  cursorPointer: css({
+    cursor: 'pointer',
+  }),
+  innerBase: css({
+    width: '100%',
+    height: '100%',
+  }),
+  shapeCircle: css({
+    borderRadius: '50%',
+  }),
+  shapeSquare: css({
+    borderRadius: '0px',
+  }),
+  shapeRound: css({
+    borderRadius: '5px',
+  }),
+  shapeLightRound: css({
+    borderRadius: '10px',
+  }),
+  shapeHeavyRound: css({
+    borderRadius: '15px',
+  }),
+  activeTypeScale: css({
+    transform: 'scale(0.8)',
+  }),
+  activeTypeOpacity: css({
+    opacity: 0.7,
+  }),
+  activeTypeBorder: css({
+    border: `2px solid #fff !important`,
+  }),
+};
 
 /**
  * カラーボックスのサイズをpropsの値から出す関数
  */
-const calcBoxSize = (boxSize: Props['boxSize']): string => {
+const calcBoxSize = (boxSize: Props['boxSize']): number => {
   switch (boxSize) {
     case 'small':
-      return '20px';
+      return 20;
     case 'medium':
-      return '30px';
+      return 30;
     case 'large':
-      return '50px';
+      return 50;
     case 'exLarge':
-      return '75px';
+      return 75;
     default:
-      return `30px`;
+      return 30;
   }
 };
 
@@ -42,67 +85,64 @@ const calcBoxSize = (boxSize: Props['boxSize']): string => {
  */
 const ColorBox: React.FC<Props> = ({
   bgColor = '#000000',
-  shapeType,
+  shapeType = 'square',
+  active = false,
   borderColor = '#ffffff',
   borderWidth = 0,
   boxSize = 'medium',
   boxSizeHeightRatio = 1,
-  boxSpacing = 0,
   onClick,
   muiBoxProps,
+  innerMuiBoxProps,
+  activeStyleType = 'scale',
 }) => {
-  const resultBoxSize = calcBoxSize(boxSize);
-  const boxSpacingUnit = 2;
+  const [isActive, setIsActive] = useState(active);
 
-  const styles: { [K: string]: SerializedStyles } = {
-    base: css`
-      cursor: ${onClick != null ? 'pointer' : 'default'};
-    `,
-    innerBase: css({
-      width: '100%',
-      height: '100%',
-    }),
-    background: css({
-      backgroundColor: bgColor,
-    }),
-    shape: css({
-      borderRadius: (() => {
-        switch (shapeType) {
-          case 'circle':
-            return '50%';
-          case 'square':
-            return '0px';
-          case 'lightRound':
-            return '5px';
-          case 'round':
-            return '10px';
-          case 'heavyRound':
-            return '15px';
-          default:
-            return '0px';
-        }
-      })(),
-    }),
-    border: css`
-      border: ${borderWidth}px solid ${borderColor};
-    `,
-    size: css`
-      width: ${resultBoxSize};
-      height: ${Number(resultBoxSize.slice(0, -2)) * boxSizeHeightRatio}px;
-    `,
-    spacing: css({
-      padding: `${boxSpacing * boxSpacingUnit}px`,
-    }),
-  };
+  const arrayActiveStyles =
+    Array.isArray(activeStyleType) && active === true
+      ? activeStyleType.map((singleActiveStyleType) => {
+          switch (singleActiveStyleType) {
+            case 'opacity':
+              return styles.activeTypeOpacity;
+            case 'scale':
+              return styles.activeTypeScale;
+            case 'border':
+              return styles.activeTypeBorder;
+            default:
+              return undefined;
+          }
+        })
+      : [];
 
   return (
     <Box
       onClick={onClick}
-      css={[styles.base, styles.size, styles.spacing]}
+      css={[onClick ? styles.cursorPointer : styles.cursorDefault]}
+      style={{
+        width: `${calcBoxSize(boxSize)}px`,
+        height: `${calcBoxSize(boxSize) * boxSizeHeightRatio}px`,
+      }}
       {...muiBoxProps}
     >
       <Box
-        css={[styles.innerBase, styles.background, styles.shape, styles.border]}
+        bgcolor={bgColor}
+        css={[
+          styles.innerBase,
+          styles.border,
+          shapeType === 'circle' && styles.shapeCircle,
+          shapeType === 'square' && styles.shapeSquare,
+          shapeType === 'round' && styles.shapeRound,
+          shapeType === 'lightRound' && styles.shapeLightRound,
+          shapeType === 'heavyRound' && styles.shapeHeavyRound,
+          isActive && activeStyleType === 'scale' && styles.activeTypeScale,
+          isActive && activeStyleType === 'opacity' && styles.activeTypeOpacity,
+          isActive && activeStyleType === 'border' && styles.activeTypeBorder,
+          ...arrayActiveStyles,
+        ]}
+        style={{
+          border: `${borderWidth}px solid ${borderColor}`,
+        }}
+        {...innerMuiBoxProps}
       />
     </Box>
   );
