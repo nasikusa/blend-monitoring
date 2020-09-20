6b71bf77dde57a84b01a2fb71f1859be26a431ac
nasikusa <youkan94@gmail.com>
2020-09-20 17:00:02 +0900

    refactor: pages系コンポーネントのフォルダ構成を変更

--- src/components/pages/Top/index.tsx
+++ src/components/pages/Top/index.tsx
@@ -1,14 +1,1 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const Top = () => {
-  return (
-    <>
-      TopPage
-      <Link to="/">Home</Link>
-      <Link to="/edit">edit</Link>
-    </>
-  );
-};
-
-export default Top;
+export { default } from './Top';
