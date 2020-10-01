66c0c4d58e4646756d2f0df9430491493146c516
nasikusa <youkan94@gmail.com>
2020-10-01 19:08:02 +0900

    style: コメントを追加

--- src/constants/general/appConstantSettings.ts
+++ src/constants/general/appConstantSettings.ts
@@ -1,8 +1,18 @@
 /**
- * アプリ名
+ * en: app name
+ * ja: アプリ名
  */
 export const AppName = `BlendMonitoring`;
+
+/**
+ * en: Bool value for beta
+ * ja: ベータ版であるかどうかのbool値
+ */
 const isBeta = true;
+
+/**
+ * 装飾情報(ベータ版であるかどうかなど）を加えたアプリ名
+ */
 export const AppNameHasAccent = isBeta ? `${AppName} β` : `${AppName}`;
 
 /**
@@ -24,7 +34,8 @@ export const AppSiteInfo = {
 };
 
 /**
- * 開発者情報
+ * en: Developer information
+ * ja: 開発者情報
  */
 export const DeveloperInfo = {
   name: `nasikusa`,
@@ -34,10 +45,15 @@ export const DeveloperInfo = {
 } as const;
 
 /**
- * 最大の追加できるglsl描画アイテムの数
+ * en: Maximum number of glsl drawing items you can add
+ * ja: 最大の追加できるglsl描画アイテムの数
  */
 export const maxCountOfGlItem = 14;
 
+/**
+ * en: Information setting object related to save data
+ * ja: セーブデータに関した情報設定のオブジェクト
+ */
 export const appSaveDataInfo = {
   extension: 'blmn',
 };
